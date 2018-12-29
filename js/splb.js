121bc760de34d034b8eb4255938273004b17d3a5
WTBIN <2976363944@qq.com>
2018-12-29 09:37:49 +0800

    ok

--- js/splb.js
+++ js/splb.js
@@ -19,31 +19,29 @@ function pbl_sjtc() {
             sjts()
         });
 
-
     //循环数据数值 添加到页面
     function sjts() {
         $.each(arr, function (i, n) {
-            // console.log(n.zt)
-            var zt=n.zt.split(",")	;
+            var zt = n.zt.split(",");
             $("#splb_body_n_splb").append(`
-            <div class="splb_body_n_splb_g">
-                    <a href="${"http://"+window.location.host+"/html/xqy.html?id="+n.id}">
-                        <div class="splb_body_n_splb_g_img">
-                            <img src="${zt[0]}"
-                                alt="">
+                    <div class="splb_body_n_splb_g">
+                            <a href="${"http://"+window.location.host+"/html/xqy.html?id="+n.id}">
+                                <div class="splb_body_n_splb_g_img">
+                                    <img src="${zt[0]}"
+                                        alt="">
+                                </div>
+                                <div class="splb_body_n_splb_g_bt">
+                                    <p>${n.bt}</p>
+                                </div>
+                                <div class="splb_body_n_splb_g_xx">
+                                    <span class="splb_body_n_splb_g_jg">￥<span id="splb_dqjg">${n.jg}</span></span>
+                                    <span class="splb_body_n_splb_g_yj">￥${n.yj}</span>
+                                    <span class="splb_body_n_splb_g_sc"><img src="//s18.mogucdn.com/p2/160908/upload_27g4f1ch6akie83hacb676j622b9l_32x30.png"
+                                            alt=""> ${n.scs}</span>
+                                </div>
+                            </a>
                         </div>
-                        <div class="splb_body_n_splb_g_bt">
-                            <p>${n.bt}</p>
-                        </div>
-                        <div class="splb_body_n_splb_g_xx">
-                            <span class="splb_body_n_splb_g_jg">￥${n.jg}</span>
-                            <span class="splb_body_n_splb_g_yj">￥${n.yj}</span>
-                            <span class="splb_body_n_splb_g_sc"><img src="//s18.mogucdn.com/p2/160908/upload_27g4f1ch6akie83hacb676j622b9l_32x30.png"
-                                    alt=""> ${n.scs}</span>
-                        </div>
-                    </a>
-                </div>
-            `)
+                 `)
         });
     }
 
@@ -57,3 +55,34 @@ $(window).scroll(function (event) {
     }
 
 });
+
+$("#splb_qdpx").click(function () {
+    splb_jgsx();
+    pbl_sjtc();
+});
+
+//价格筛选
+function splb_jgsx() {
+    //最小价格
+    var zx = $("#splb_qsj").val() * 1;
+    //最大价格
+    var zd = $("#splb_wsj").val() * 1;
+
+    //大小检擦
+    if (zd < zx) {
+        var x = zx;
+        zx = zd;
+        zd = x;
+    }
+
+    //循环商品列表将不符合条件的隐藏
+    $.each($(".splb_body_n_splb_g"), function (i, n) {
+        //当前商品价格
+        var dj = $(n).find("#splb_dqjg").text() * 1
+        if (dj < zx || dj > zd) {
+            $(n).css("display", "none");
+        } else {
+            $(n).css("display", "inline-block");
+        }
+    });
+}
